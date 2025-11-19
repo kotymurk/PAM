c2729730a95c336425d81b73171eebdef55f5d4c
kotymurk <kotymurk@vk.com>
2025-11-19 16:31:19 +0200

    find

--- js/script.js
+++ js/script.js
@@ -22,3 +22,18 @@ burger.addEventListener("click", () => {
   burger.classList.toggle("active");
   menu.classList.toggle("active");
 });
+
+document.querySelectorAll(".city").forEach((city) => {
+  city.addEventListener("click", () => {
+    const id = city.dataset.city;
+
+    document.querySelectorAll(".info__bg").forEach((card) => {
+      card.classList.remove("active");
+    });
+
+    const current = document.querySelector("#info-" + id);
+    if (current) {
+      current.classList.add("active");
+    }
+  });
+});
